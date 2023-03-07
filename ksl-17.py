e552ec60b6f26d01b9e2d601b5e63c73df1820ef
eki8 <ekislykh@gmail.com>
2023-03-06 22:42:13 -0500

    prog 17 upd

--- ksl-17.py
+++ ksl-17.py
@@ -11,6 +11,8 @@ def blue(slice, a, b, c, d):
     slice[a:b, c:d, 2] = 255
     return slice
 
+name = input("Enter file name: ")
+
 img = np.zeros((30,30,3))
 img[:, :, 1] = 255
 img[:, :, 0] = 255
@@ -19,6 +21,8 @@ img = blue(img, 3, 26, 4, 7)
 img = blue(img, 13, 16, 4, 26)
 img = blue(img, 24, 27, 4, 26)
 img = blue(img, 15, 27, 23, 26)
+
+plt.imsave(name, img)
 
 #plt.imshow(img)
 #plt.show()
