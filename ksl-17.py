24ea347a9061d25e4e93d1706883557451f52da4
eki8 <ekislykh@gmail.com>
2023-03-06 22:36:21 -0500

    program 17

---
+++ ksl-17.py
@@ -0,0 +1,24 @@
+#Name: Egor Kislykh
+#Email: egor.kislykh24@myhunter.cuny.edu
+#Date: Mar 6, 2023
+#This is program number 17
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def blue(slice, a, b, c, d):
+    slice[a:b, c:d, 0:2] = 0
+    slice[a:b, c:d, 2] = 255
+    return slice
+
+img = np.zeros((30,30,3))
+img[:, :, 1] = 255
+img[:, :, 0] = 255
+
+img = blue(img, 3, 26, 4, 7)
+img = blue(img, 13, 16, 4, 26)
+img = blue(img, 24, 27, 4, 26)
+img = blue(img, 15, 27, 23, 26)
+
+#plt.imshow(img)
+#plt.show()
